f30bf6b321335fab8806ea95105b8c4040fb4577
Gustavo <gugu.soliveira2009@gmail.com>
2026-09-13 13:24:51 -0300

    fix(build): preserva tamanho do icon button

--- designSystem/src/main/java/buttons/QuimiaIconButton.kt
+++ designSystem/src/main/java/buttons/QuimiaIconButton.kt
@@ -31,7 +31,9 @@ fun QuimiaIconButton(
     iconSize: Dp,
     contentDescription: String? = null,
     padding: Dp = PaddingMedium.dp,
-    radius: Dp = RadiusFull.dp
+    radius: Dp = RadiusFull.dp,
+    width: Dp = 1.dp,
+    buttonSize: Dp? = null
 ) {
     val tokens = quimiaColorTokens()
     val actualContainer = containerColor ?: tokens.primary
@@ -53,6 +55,7 @@ fun QuimiaIconButton(
             tint = actualIconColor,
             modifier = Modifier
                 .size(iconSize)
+                .width(width)
         )
     }
 }
